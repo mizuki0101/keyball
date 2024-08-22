95e2b4c01479dfa5e3f6af7c1a469f11744b6f91
mizuki0101 <gresky11@gmail.com>
2024-08-23 08:54:54 +0900

    fix(keyball39): aml disable  ACT_LAYER

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/mdk/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/mdk/keymap.c
@@ -66,7 +66,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
         // checks highest layer other than target layer
     switch(get_highest_layer(remove_auto_mouse_layer(state, true))) {
-        case _LAYER3:
+        case ACT_LAYER3:
             // remove_auto_mouse_target must be called to adjust state *before* setting enable
             state = remove_auto_mouse_layer(state, false);
             set_auto_mouse_enable(false);
@@ -75,7 +75,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             set_auto_mouse_enable(true);
             break;
     }
-    
+
     return state;
 }
 
