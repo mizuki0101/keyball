5fc4a69ed2ab4018d15745a7a44262f5f861866c
mizuki0101 <gresky11@gmail.com>
2024-08-25 17:21:51 +0900

    add(keyball39): combokey

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/mdk/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/mdk/keymap.c
@@ -24,10 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINS  ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(2,KC_M)     , KC_BTN1  , KC_BTN2   , KC_SLSH  ,
-    KC_LCTL  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_ENT, KC_RSFT
+    C_S_T(KC_Q)     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , C_S_T(KC_P)     ,
+    LCTL_T(KC_A)     , KC_S     , KC_D     , LT(3,KC_F)     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_BSPC  ,
+    LSFT_T(KC_Z)     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , LT(2,KC_M)     , KC_BTN1  , KC_BTN2   , KC_SLSH  ,
+    KC_LCTL  , KC_LALT  , KC_LGUI  ,LT(1,KC_TAB),LT(2,KC_SPC),LT(3,KC_ESCAPE),_____,_____,_____,KC_RALT,KC_ENT, KC_RSFT
   ),
 
   [1] = LAYOUT_universal(
@@ -108,3 +108,9 @@ void pointing_device_init_user(void) {
 //             break;
 //     }
 //     #endif
+
+const uint16_t PROGMEM esc_combo1[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM test_combo2[] = {KC_C, KC_D, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(esc_combo1, KC_ESC),
+};
