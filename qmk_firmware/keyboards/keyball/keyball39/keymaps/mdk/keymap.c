45728f050fcdda7ced5984582469b1f7c145cef3
mizuki0101 <gresky11@gmail.com>
2024-09-03 14:22:46 +0900

    add(keyball39):  macro shift x_end  fix

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/mdk/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/mdk/keymap.c
@@ -81,7 +81,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     //         // when keycode QMKURL is released
     //     }
     //     break;
-    // }
+    }
     return true;
 };
 
